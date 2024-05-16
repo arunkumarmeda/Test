3466194022b37864b85696f641af2e8fa78cec72
arunkumarmeda <arunkumar0aa@gmail.com>
2024-05-16 19:54:52 +0530

    Odd.javacommited

---
+++ Odd.java
@@ -0,0 +1,15 @@
+public class Even{
+public static void main(String[] args)
+{
+ int a = 9;
+
+if(a%2==0)
+{
+System.out.println("EvenNumber");
+}
+else
+{
+System.out.println("OddNumber");
+}
+}
+}
